f282535aa748000836f2bb3a50719549ba3633e5
Xuettttt <ai.zxt19990330@gmail.com>
2025-03-31 20:19:28 +1100

    fianlize resume

--- Portfolio/portfolio/src/components/Resume/index.jsx
+++ Portfolio/portfolio/src/components/Resume/index.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react';
+import resumePDF from '../../Assets/resume.pdf';
 
 function Resume() {
   return (
-    <div>
-      <h1>Resume Page</h1>
+    <div style={{ textAlign: 'center', padding: '40px', display: "flex", justifyContent: "center", alignItems: "center", marginTop: "60px"}}>
+      <iframe 
+        src= {resumePDF}
+        width="80%"
+        height="600px"
+        style={{border: "none"}}
+        title="resume"
+      >
+      </iframe>
     </div>
-  )
+  );
 }
 
 export default Resume;
